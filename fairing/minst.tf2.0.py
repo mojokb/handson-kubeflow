13e5a2f57105dacb7e9499db0a5823c0acbb241e
Lee MyoungHwan <amaramusic@gmail.com>
2019-10-23 16:41:51 +0900

    Update minist.tf2.0.py

--- fairing/minst.tf2.0.py
+++ fairing/minst.tf2.0.py
@@ -1,21 +1,27 @@
 import tensorflow as tf
 
-mnist = tf.keras.datasets.mnist
+class MyModel(object):
+  def train(self):
+    mnist = tf.keras.datasets.mnist
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation='softmax')
-])
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dropout(0.2),
+      tf.keras.layers.Dense(10, activation='softmax')
+    ])
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+    model.fit(x_train, y_train, epochs=5)
 
-model.evaluate(x_test,  y_test, verbose=2)
+    model.evaluate(x_test,  y_test, verbose=2)
+
+if __name__ == '__main__':
+    local_train = MyModel()
+    local_train.train()
