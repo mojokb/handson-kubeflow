7ea83f6e47241fc2ecfcbe876be4710e36ed1769
Lee MyoungHwan <amaramusic@gmail.com>
2019-10-25 03:32:04 +0900

    fairing 소스 추가, pipeline용 mnist 관련 추가

---
+++ fairing/fairing_mnist.tf2.0.dockerhub.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+import random
+import os
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+class MyModel(object):
+    def train(self):
+        mnist = tf.keras.datasets.mnist
+
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_train, x_test = x_train / 255.0, x_test / 255.0
+
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(28, 28)),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation='softmax')
+        ])
+
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(x_train, y_train, epochs=5)
+
+        model.evaluate(x_test, y_test, verbose=2)
+
+
+if __name__ == '__main__':
+    if os.getenv('FAIRING_RUNTIME', None) is None:
+        from kubeflow import fairing
+        from kubeflow.fairing.kubernetes import utils as k8s_utils
+
+        DOCKER_REGISTRY = 'brightfly'
+        fairing.config.set_builder(
+            'append',
+            base_image='gcr.io/kubeflow-images-public/tensorflow-1.13.1-notebook-cpu:v0.5.0',
+            registry=DOCKER_REGISTRY, push=True)
+        # cpu 1, memory 1GiB
+        fairing.config.set_deployer('job',
+                                    pod_spec_mutators=[
+                                        k8s_utils.get_resource_mutator(cpu=1,
+                                                                       memory=1)]
+                                    )
+        fairing.config.set_preprocessor('python', input_files=[__file__])
+        fairing.config.run()
+    else:
+        remote_train = MyModel()
+        remote_train.train()
--- pipeline/mnist_pipeline.py
+++ pipeline/mnist_pipeline.py
@@ -4,80 +4,76 @@ import kfp.onprem as onprem
 
 platform = 'onprem'
 
+
 @dsl.pipeline(
-  name='MNIST',
-  description='A pipeline to train and serve the MNIST example.'
+    name='MNIST',
+    description='A pipeline to train and serve the MNIST example.'
 )
 def mnist_pipeline(model_export_dir='gs://your-bucket/export',
                    train_steps='200',
                    learning_rate='0.01',
                    batch_size='100',
                    pvc_name=''):
-  """
-  Pipeline with three stages:
-    1. train an MNIST classifier
-    2. deploy a tf-serving instance to the cluster
-    3. deploy a web-ui to interact with it
-  """
-  train = dsl.ContainerOp(
-      name='train',
-      image='gcr.io/kubeflow-examples/mnist/model:v20190304-v0.2-176-g15d997b',
-      arguments=[
-          "/opt/model.py",
-          "--tf-export-dir", model_export_dir,
-          "--tf-train-steps", train_steps,
-          "--tf-batch-size", batch_size,
-          "--tf-learning-rate", learning_rate
-          ]
-  )
+    """
+    Pipeline with three stages:
+      1. train an MNIST classifier
+      2. deploy a tf-serving instance to the cluster
+      3. deploy a web-ui to interact with it
+    """
+    train = dsl.ContainerOp(
+        name='train',
+        image='gcr.io/kubeflow-examples/mnist/model:v20190304-v0.2-176-g15d997b',
+        arguments=[
+            "/opt/model.py",
+            "--tf-export-dir", model_export_dir,
+            "--tf-train-steps", train_steps,
+            "--tf-batch-size", batch_size,
+            "--tf-learning-rate", learning_rate
+        ]
+    )
 
+    serve_args = [
+        '--model-export-path', model_export_dir,
+        '--server-name', "mnist-service"
+    ]
+    if platform != 'GCP':
+        serve_args.extend([
+            '--cluster-name', "mnist-pipeline",
+            '--pvc-name', pvc_name
+        ])
 
-  serve_args = [
-      '--model-export-path', model_export_dir,
-      '--server-name', "mnist-service"
-  ]
-  if platform != 'GCP':
-    serve_args.extend([
-        '--cluster-name', "mnist-pipeline",
-        '--pvc-name', pvc_name
-    ])
+    serve = dsl.ContainerOp(
+        name='serve',
+        image='gcr.io/ml-pipeline/ml-pipeline-kubeflow-deployer:'
+              'e9b96de317989a9673ef88d88fb9dab9dac3005f',
+        arguments=serve_args
+    )
+    serve.after(train)
 
-  serve = dsl.ContainerOp(
-      name='serve',
-      image='gcr.io/ml-pipeline/ml-pipeline-kubeflow-deployer:'
-            '7775692adf28d6f79098e76e839986c9ee55dd61',
-      arguments=serve_args
-  )
-  serve.after(train)
+    web_ui = dsl.ContainerOp(
+        name='web-ui',
+        image='brightfly/kubeflow-deploy-service:handson',
+        arguments=[
+            '--image',
+            'gcr.io/kubeflow-examples/mnist/web-ui:v20190304-v0.2-176-g15d997b-pipelines',
+            '--name', 'web-ui',
+            '--container-port', '5000',
+            '--service-port', '80',
+            '--service-type', "LoadBalancer",
+            '--cluster-name', "mnist-pipeline"
+        ]
+    )
+    web_ui.after(serve)
 
+    steps = [train, serve, web_ui]
+    for step in steps:
+        if platform == 'GCP':
+            step.apply(gcp.use_gcp_secret('user-gcp-sa'))
+        else:
+            step.apply(onprem.mount_pvc(pvc_name, 'local-storage', '/mnt'))
 
-  webui_args = [
-          '--image', 'gcr.io/kubeflow-examples/mnist/web-ui:'
-                     'v20190304-v0.2-176-g15d997b-pipelines',
-          '--name', 'web-ui',
-          '--container-port', '5000',
-          '--service-port', '80',
-          '--service-type', "LoadBalancer"
-  ]
-  if platform != 'GCP':
-    webui_args.extend([
-      '--cluster-name', "mnist-pipeline"
-    ])
-
-  web_ui = dsl.ContainerOp(
-      name='web-ui',
-      image='gcr.io/kubeflow-examples/mnist/deploy-service:latest',
-      arguments=webui_args
-  )
-  web_ui.after(serve)
-
-  steps = [train, serve, web_ui]
-  for step in steps:
-    if platform == 'GCP':
-      step.apply(gcp.use_gcp_secret('user-gcp-sa'))
-    else:
-      step.apply(onprem.mount_pvc(pvc_name, 'local-storage', '/mnt'))
 
 if __name__ == '__main__':
-  import kfp.compiler as compiler
-  compiler.Compiler().compile(mnist_pipeline, __file__ + '.tar.gz')
+    import kfp.compiler as compiler
+
+    compiler.Compiler().compile(mnist_pipeline, __file__ + '.tar.gz')
