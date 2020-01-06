c58e9068e34e565507214dd3c97fb67c2d6b3972
mojokb <amaramusic@gmail.com>
2020-01-06 18:17:30 +0000

    Add kfserving mnist-pipieline

--- pipeline/mnist_pipeline_kfserving.py
+++ pipeline/mnist_pipeline_kfserving.py
@@ -1,6 +1,5 @@
 import kfp
 import kfp.dsl as dsl
-import kfp.onprem as onprem
 
 def echo_op(text):
     return dsl.ContainerOp(
@@ -10,23 +9,23 @@ def echo_op(text):
         arguments=['echo "$0"', text],
     )
 
-
 @dsl.pipeline(
     name='MnistPipeline',
     description='mnist '
 )
-def mnist_pipeline(learning_rate, dropout_rate, checkpoint_dir, 
-                   saved_model_dir, tensorboard_log, namespace, storage_uri, name):
+def mnist_pipeline(volume_size, learning_rate, dropout_rate, checkpoint_dir,
+                   saved_model_dir, tensorboard_log, namespace, storage_uri,
+                   name):
     exit_task = echo_op("Done!")
-    with dsl.ExitHandler(exit_task):    
+    with dsl.ExitHandler(exit_task):
         vop = dsl.VolumeOp(
-            name="mnist_model_volume",
-            resource_name="mnist_model",
+            name='mnist_model',
+            resource_name='mnist_model',
             storage_class="nfs-client",
             modes=dsl.VOLUME_MODE_RWM,
-            size="10Gi"
+            size=volume_size
         )
-        
+
         mnist = dsl.ContainerOp(
             name='Mnist',
             image='kubeflow-registry.default.svc.cluster.local:30000/katib-job:FF61F3B',
@@ -40,46 +39,40 @@ def mnist_pipeline(learning_rate, dropout_rate, checkpoint_dir,
             ],
             pvolumes={"/result": vop.volume}
         )
-        
+
         result = dsl.ContainerOp(
             name='list_list',
             image='library/bash:4.4.23',
             command=['ls', '-R', '/result'],
             pvolumes={"/result": mnist.pvolume}
         )
-
         kfserving = dsl.ContainerOp(
             name='kfserving',
-            image='kubeflow-registry.default.svc.cluster.local:30000/kfserving:50705CEF',
+            image='kubeflow-registry.default.svc.cluster.local:30000/kfserving:6D7B836C',
             command=['python', '/app/kfserving-fairing.py'],
             arguments=[
-                "--namespace", namespace,                
-                "--storage_uri", storage_uri,
+                "--namespace", namespace,
+                "--storage_uri", "pvc://" + str(vop.volume.persistent_volume_claim.claim_name) + str(storage_uri),
                 "--name", name
             ],
-            pvolumes={"/result": mnist.pvolume}   
+            pvolumes={"/result": mnist.pvolume}
         )
-        
-        # parser.add_argument('--namespace', required=False, default='kubeflow')
-        # file://blah/blah
-        # parser.add_argument('--storage_uri', required=False, default='file://mnt/export')
-        # parser.add_argument('--name', required=False, default='kfserving-sample')     
-        
         mnist.after(vop)
         result.after(mnist)
         kfserving.after(mnist)
-    
+        kfserving.after(vop)
 
-arguments = {'learning_rate': '0.01',
+arguments = {'volume_size' : '5Gi',
+             'learning_rate': '0.01',
              'dropout_rate': '0.2',
              'checkpoint_dir': '/result/training_checkpoints',
-             'saved_model_dir':'/result/saved_model/0001',
+             'saved_model_dir': '/result/saved_model/0001',
              'tensorboard_log': '/result/log',
-             'namespace' : 'kubeflow',
-             'storage_uri' : '/result/saved_model',
-             'name' : 'akfs-test1'
-            }
-    
+             'namespace': 'kubeflow',
+             'storage_uri': '/saved_model',
+             'name': 'kfserving-mnist-01'
+             }
+
 if __name__ == '__main__':
-    kfp.Client().create_run_from_pipeline_func(pipeline_func=mnist_pipeline, 
+    kfp.Client().create_run_from_pipeline_func(pipeline_func=mnist_pipeline,
                                                arguments=arguments)
