06af89b8bae3ce3c7986f882b7f5dc28f07bfb26
LeeMyoungHwan <amaramusic@gmail.com>
2020-04-07 18:20:45 +0000

    Update pipeline

--- pipeline/hello_kubeflow.py
+++ pipeline/hello_kubeflow.py
@@ -14,5 +14,9 @@ def hellokubelfow_pipeline():
     )
 
 if __name__ == '__main__':
-    kfp.Client().create_run_from_pipeline_func(pipeline_func=hellokubelfow_pipeline, 
-                                               arguments={})
+    kfp.Client().create_run_from_pipeline_func(pipeline_func=hellokubelfow_pipeline, arguments={})
+
+    # 파이프라인 파일로 등록시
+    # kfp.compiler.Compiler().compile(hellokubelfow_pipeline, 'containerop.pipeline.tar.gz')    
+    # dsl-compile 툴을 이용할 경우
+    # $ dsl-compile --py containerop.py --output containerop.pipeline.tar.gz
